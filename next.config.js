70ae67dfcbb1b096dacf8c971e709f8c8df99c4b
GTYaseen <Xxyaseent11xX14@gmail.com>
2024-01-15 23:18:11 +0300

    test2

--- next.config.js
+++ next.config.js
@@ -11,22 +11,17 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // matching all API routes for login and register
         source: "/api/(login*|register*)",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://star-food-bay.vercel.app",
-          },
+          { key: "Access-Control-Allow-Origin", value: "https://star-food-bay.vercel.app" },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,DELETE,PATCH,POST,PUT",
           },
           {
             key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
         ],
       },
