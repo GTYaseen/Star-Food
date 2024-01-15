ed0afb2836ee1689a14d8a026fdc029a05c20426
GTYaseen <Xxyaseent11xX14@gmail.com>
2024-01-15 22:49:02 +0300

    test online

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -81,7 +81,7 @@ function Page() {
 
   const handleRegister = async () => {
     try {
-      const response = await axios.post("http://localhost:3000/api/register", {
+      const response = await axios.post("https://star-food-bay.vercel.app/api/register", {
         name,
         password,
         phoneNumber,
@@ -105,7 +105,7 @@ function Page() {
   };
   const handleLogin = async () => {
     try {
-      const response = await axios.post("http://localhost:3000/api/login", {
+      const response = await axios.post("https://star-food-bay.vercel.app/api/login", {
         username,
         password,
       });
