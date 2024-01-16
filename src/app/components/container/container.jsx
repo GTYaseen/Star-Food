704fd83573c180724de43cf75cff30442e8694e2
GTYaseen <128807198+GTYaseen@users.noreply.github.com>
2024-01-16 20:46:15 +0300

    test

--- src/app/components/container/container.jsx
+++ src/app/components/container/container.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 
-function Container({ children, width, ...props }) {
-  if (!width){
-    const widthh = 1000;
-  }else{
-    const widthh = width
-  }
+function AppContainer({ children, width=1000, ...props }) {
   return (
-    <div className={`w-full max-w-[${widthh}px] mx-auto my-auto`} {...props}>
+    <div className={`w-full max-w-[1000px] mx-auto my-auto`} {...props}>
       {children}
     </div>
   );
 }
-export default Container;
+export default AppContainer;
 
