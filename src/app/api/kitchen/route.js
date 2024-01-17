46e2b8c999d63e9ffee99c91a20491f4942d813c
GTYaseen <128807198+GTYaseen@users.noreply.github.com>
2024-01-18 00:37:30 +0300

    Update route.js

--- src/app/api/kitchen/route.js
+++ src/app/api/kitchen/route.js
@@ -1,30 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 const prisma = new PrismaClient();
-function setCorsHeaders(response) {
-  response.headers.set("Access-Control-Allow-Origin", "*");
-  response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  response.headers.set(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization"
-  );
-  return response;
-}
 
 export async function GET(req) {
   try {
-    return setCorsHeaders(
-      NextResponse.json({
-        success: true,
-        kitchens: await prisma.kitchen.findMany(),
-      })
-    );
+    return NextResponse.json({
+      success: true,
+      kitchens: await prisma.kitchen.findMany(),
+    });
   } catch (error) {
-    return setCorsHeaders(
-      NextResponse.json({
-        success: false,
-        error: error.message,
-      })
-    );
+    return NextResponse.json({
+      success: false,
+      error: error.message,
+    })
   }
 }
