1d366e953ff00494d59c42b219ba76000ab080f1
TabarakAli <151866250+TabarakCodeCraft@users.noreply.github.com>
2024-01-20 02:13:09 +0300

    test

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Navpar from "../components/header/Navpar";
 import AppContainer from "../components/container/container";
-import { FaStar } from "react-icons/fa";
 import { FaRegStar } from "react-icons/fa";
 import { Space } from "@/app/components/space/Space";
-import { GiPointing } from "react-icons/gi";
+import { LuChefHat } from "react-icons/lu";
 function About() {
   return (
     <>
-    {/* لا توجد عبقرية دون مسحة الجنون */}
+      {/* لا توجد عبقرية دون مسحة الجنون */}
       <Navpar />
       <Space height={"3rem"} />
       <AppContainer>
@@ -24,7 +23,35 @@ function About() {
             الاطباق التقليدية والمطورة بطريقة مميزة ستدمن تجربة تلك الاطباق من
             أول مرة <FaRegStar />
           </p>
-          
+        </div>
+      </AppContainer>
+      <Space height={"2px"} />
+      <AppContainer>
+        <div
+          style={{
+            textAlign: "center",
+            backgroundColor: "#FFF0A2",
+            padding: "10px",
+          }}
+        >
+          <p className="text-3xl text-shadow "> س/ ماهي ميزة المشروع ؟</p>
+          <h1 className="text-2xl text-shadow">
+            تم تصميم هذا التطبيق ليضم اصحاب مطابخ تحب عرض عملها و وصفاتها <br />
+            سيكون لكل مطبخ صفحة تضم مايحب عرضه بشكل مميز <LuChefHat />
+          </h1>
+        </div>
+      </AppContainer>
+      <Space height={"2px"} />
+      <AppContainer>
+        <div
+          style={{
+            textAlign: "center",
+            backgroundColor: "#FFF0A2",
+            padding: "10px",
+          }}
+        >
+          <p className="text-3xl text-shadow "> س/ من هو صاحب المشروع ؟</p>
+          <h1 className="text-4xl text-shadow">?</h1>
         </div>
       </AppContainer>
     </>
