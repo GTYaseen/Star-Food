8292199d1f943152b6dc0f2637dcddd6d70b210b
TabarakAli <151866250+TabarakCodeCraft@users.noreply.github.com>
2024-02-09 21:39:46 +0300

    Update sideMenu

--- src/app/components/header/sideMenu/sideMenu.jsx
+++ src/app/components/header/sideMenu/sideMenu.jsx
@@ -9,6 +9,7 @@ import { PiSignOutBold } from "react-icons/pi";
 import { LuChefHat } from "react-icons/lu";
 import { MdDeliveryDining } from "react-icons/md";
 import { AiOutlineHome } from "react-icons/ai";
+import { IoIosArrowBack } from "react-icons/io";
 import useStore from "@/app/store";
 
 function SideMenu({ isMenuOpen, setIsMenuOpen, favoriteKitchens }) {
@@ -35,20 +36,16 @@ function SideMenu({ isMenuOpen, setIsMenuOpen, favoriteKitchens }) {
     location.reload();
   };
 
-  const handleGoBack = () => {
-    window.history.back();
-  };
-
   return (
     <>
-      {/* Overlay */}
+
       <div
         className={`fixed top-0 left-0 w-full h-full bg-gray-800 z-40 duration-300 opacity-50 ${
           isMenuOpen ? "block" : "hidden"
         }`}
         onClick={sideClick}
       ></div>
-      {/* Side Menu */}
+      
       <div
         className={`fixed top-0 right-0 h-full bg-white z-50 duration-500 border-l-2 transition-margin-right ${
           isMenuOpen ? "w-[500px]" : "w-0"
@@ -59,7 +56,10 @@ function SideMenu({ isMenuOpen, setIsMenuOpen, favoriteKitchens }) {
         >
           <div className="flex border-b-2 items-center justify-between">
             <div className="bg-white h-12 flex items-center justify-center p-4">
-              <PiSignOutBold />
+              <IoIosArrowBack
+                className="text-2xl cursor-pointer"
+                onClick={sideClick}
+              />
             </div>
             <div className="bg-white h-22 flex items-center justify-end p-4 px-4">
               <p className="text-3xl font-normal drop-shadow-lg">{name}</p>
@@ -75,7 +75,7 @@ function SideMenu({ isMenuOpen, setIsMenuOpen, favoriteKitchens }) {
             <ul className="flex flex-col gap-5 p-4 text-2xl items-end justify-end mr-7 ">
               <li className="flex items-center cursor-pointer">
                 <Link href="/" className="flex items-center">
-                  الصفحه الرئيسيه
+                الصفحة الرئيسية
                   <Space width={"5px"} />
                   <AiOutlineHome />
                 </Link>
@@ -150,15 +150,3 @@ function SideMenu({ isMenuOpen, setIsMenuOpen, favoriteKitchens }) {
 }
 
 export default SideMenu;
-
-{
-  /* <div className="bg-white h-22 flex items-center justify-between p-4 px-7">
-<div
-  className="flex items-center cursor-pointer"
-  onClick={handleGoBack}
->
-  <FaArrowRight className="text-3xl" />
-  <p className="text-2xl font-normal drop-shadow-lg ml-2">رجوع</p>
-</div>
-</div> */
-}
